921fd940aa18ef2d3dd54a15201ecf795882b01a
toshinori.chiba <toshinori.chiba.p@gmail.com>
2024-02-07 08:50:27 +0900

    add SoftmaxFunctionTest

---
+++ src/test/scala/activationfunction/SoftmaxFunctionTest.scala
@@ -0,0 +1,33 @@
+package activationfunction
+
+import base.BaseTest
+import matrix.Matrix
+
+class SoftmaxFunctionTest extends BaseTest {
+
+  describe("SoftmaxFunctionTest") {
+    describe("apply") {
+      it("ソフトマックス関数の出力は各要素0以上1以下かつ、その合計は1になる") {
+        val input  = Matrix(Array(Array(1.0, 2.0, 3.0)))
+        val output = SoftmaxFunction(input)
+        assert(output(0).forall(x => x >= 0 && x <= 1))
+        output(0).sum shouldBe 1.0 +- 0.00001
+      }
+
+      it("2行以上のの行列には対応できない") {
+        val input = Matrix(Array(Array(1.0, 2.0, 3.0), Array(4.0, 5.0, 6.0)))
+        intercept[IllegalArgumentException] {
+          SoftmaxFunction(input)
+        }
+      }
+    }
+
+    describe("apply1") {
+      it("apply1を呼ぼうとすると,UnsupportedOperationExceptionが発生する") {
+        intercept[UnsupportedOperationException] {
+          SoftmaxFunction.apply1(1.0)
+        }
+      }
+    }
+  }
+}
